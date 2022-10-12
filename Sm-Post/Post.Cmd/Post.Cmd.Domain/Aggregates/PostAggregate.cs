de317b11b3aa27bb825edf8222ecb442fd0db2fe
kevin <hyunbin7303@gmail.com>
2022-10-11 23:46:43 -0400

    Update Remove  comment post aggreagate

--- Sm-Post/Post.Cmd/Post.Cmd.Domain/Aggregates/PostAggregate.cs
+++ Sm-Post/Post.Cmd/Post.Cmd.Domain/Aggregates/PostAggregate.cs
@@ -55,7 +55,7 @@ namespace Post.Cmd.Domain.Aggregates
         {
             if(_active)
             {
-                throw new InvalidOperationException("You cabnnot like an Inactive post!");
+                throw new InvalidOperationException("You cannot like an Inactive post!");
             }
             RaiseEvent(new PostLikedEvent{
                 Id = _id
@@ -64,6 +64,58 @@ namespace Post.Cmd.Domain.Aggregates
         public void Apply(PostLikedEvent @event)
         {
             _id = @event.Id;
+        }
+        public void AddComment(string comment, string username)
+        {
+            if(!_active)
+            {
+                throw new InvalidOperationException("You cannot like an Inactive post!");
+            }
+            if(string.IsNullOrWhiteSpace(comment))
+            {
+                throw new InvalidOperationException($"The value of {nameof(comment)} cannot be null or empty. Please provide a valid {nameof(comment)}");
+            }
+            RaiseEvent(new  CommentAddedEvent{
+                Id = _id,
+                CommentId = Guid.NewGuid(),
+                Comment = comment,
+                Username = username, 
+                CommentDate = DateTime.Now
+            });
+        }
+        public void Apply(CommentAddedEvent @event){
+            _id = @event.Id;
+            _comments.Add(@event.CommentId, new Tuple<string, string>(@event.Comment, @event.Username));
+        }
+        public void EditComment(Guid commentId, string comment, string username)
+        {
+            if (!_active)
+            {
+                throw new InvalidOperationException("You cannot edit a comment of an Inactive post!");
+            }
+            if(!_comments[commentId].Item2.Equals(username, StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new InvalidOperationException("You are not allowed to edit a comment that was made by another user!")
+            }
+            RaiseEvent(new CommentUpdatedEvent{
+                Id = _id,
+                CommentId = commentId,
+                Comment = comment,
+                Username = username,
+                EditDate = DateTime.Now
+            });
+        }
+        public void Apply(CommentUpdatedEvent @event)
+        {
+            _id = @event.Id;
+            _comments[@event.CommentId] = new Tuple<string, string>(@event.Comment, @event.Username);
+        }
+        public void RemoveComment(Guid commentId, string username)
+        {
+            if (!_active)
+            {
+                throw new InvalidOperationException("You cannot remove a comment of an Inactive post!");
+            }
         }
     }
 }
