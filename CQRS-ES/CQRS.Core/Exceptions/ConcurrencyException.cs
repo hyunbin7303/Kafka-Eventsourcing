40f5c0bf0cd90d9bb90f4372783672a4a574dcf7
kevin <hyunbin7303@gmail.com>
2022-10-13 23:42:03 -0400

    Event Sourcing handler update

--- CQRS-ES/CQRS.Core/Exceptions/ConcurrencyException.cs
+++ CQRS-ES/CQRS.Core/Exceptions/ConcurrencyException.cs
@@ -2,9 +2,5 @@ namespace CQRS.Core.Exceptions
 {
     public class ConcurrencyException : Exception
     {
-        public ConcurrencyException(string msg) : base(msg)
-        {
-            
-        }
     }
 }
--- CQRS-ES/CQRS.Core/Infrastructure/IEventStore.cs
+++ CQRS-ES/CQRS.Core/Infrastructure/IEventStore.cs
@@ -2,7 +2,7 @@ using CQRS.Core.Events;
 
 namespace CQRS.Core.Infrastructure
 {
-    public class IEventStore
+    public interface IEventStore
     {
         Task SaveEventsAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectedVersion);
         Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId);
