319d74c49fd5d368d2817ad5b67283ad18ee76da
kevin <hyunbin7303@gmail.com>
2022-10-15 12:12:22 -0400

    DI - Register Handler

--- Sm-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ Sm-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -4,6 +4,7 @@ using CQRS.Core.Infrastructure;
 using Post.Cmd.Api.Commands;
 using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
 using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
@@ -15,6 +16,18 @@ builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
 builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Register cmdhandler emethods
+var cmdHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispather = new CommandDispatcher();
+dispather.RegisterHandler<NewPostCommand>(cmdHandler.HandlerAsync);
+dispather.RegisterHandler<EditMessageCommand>(cmdHandler.HandlerAsync);
+dispather.RegisterHandler<LikePostCommand>(cmdHandler.HandlerAsync);
+dispather.RegisterHandler<AddCommentCommand>(cmdHandler.HandlerAsync);
+dispather.RegisterHandler<EditCommentCommand>(cmdHandler.HandlerAsync);
+dispather.RegisterHandler<RemoveCommentCommand>(cmdHandler.HandlerAsync);
+dispather.RegisterHandler<DeletePostCommand>(cmdHandler.HandlerAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispather);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
