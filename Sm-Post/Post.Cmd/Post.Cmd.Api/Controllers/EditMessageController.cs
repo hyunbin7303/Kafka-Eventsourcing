32c2eee2d91613f48c528ec59cef81d540a897ed
kevin <hyunbin7303@gmail.com>
2022-12-02 09:34:01 -0500

    Create EditMessageController.cs

---
+++ Sm-Post/Post.Cmd/Post.Cmd.Api/Controllers/EditMessageController.cs
@@ -0,0 +1,65 @@
+﻿using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class EditMessageController : ControllerBase
+    {
+
+        private readonly ILogger<EditMessageController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+        public EditMessageController(ILogger<EditMessageController> logger, ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditMessageAsync(Guid id, EditMessageCommand cmd)
+        {
+            try
+            {
+                cmd.Id = id;
+                await _commandDispatcher.SendAsync(cmd);
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Edit msg request completed successfully."
+                });
+            }
+            catch(InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+
+            catch (AggregateNotFoundException  ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Could not retrieve aggregate, client passed a incorrect post ID targetting the aggregate.");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (Exception e)
+            {
+                const string SAFE_ERROR_MSG = "Error while processing request to edit the msg of a post!";
+                _logger.Log(LogLevel.Error, e, SAFE_ERROR_MSG);
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MSG
+                });
+            }
+
+        }
+
+    }
+}
